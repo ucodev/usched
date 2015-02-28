0b22bb826cedec27f9bea9daba7e6fc4d826e103
Pedro A. Hortas <pah@ucodev.org>
2015-02-28 18:00:43 +0000

    v0.09u4

--- tests/test_splint.sh
+++ tests/test_splint.sh
@@ -1,80 +1,94 @@
 #!/bin/sh
 
+# Globals
+SPLINT_OPTS="-posix-lib -weak -redef -fcnuse -unrecog"
+SPLINT_OPTS_EXTRA="+ignorequals -duplicatequals"
+SPLINT_OUT=".splint_out"
+
+DEFINES="-D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned"
+INCLUDES="-I../include"
+
 printf "Analyzing uSched source code with splint...\n\n"
 
+# Analyze src/common
 printf " * Checking src/common... "
 for file in ../src/common/*.c; do
-	splint -I../include -DCONFIG_COMMON=1 -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} -DCONFIG_COMMON=1 ${SPLINT_OPTS} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
 echo "OK"
 
+# Analyze src/usa
 printf " * Checking src/usa... "
 for file in ../src/usa/*.c; do
-	splint -I../include -DCONFIG_ADMIN_SPECIFIC=1 -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} -DCONFIG_ADMIN_SPECIFIC=1 ${SPLINT_OPTS} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
 echo "   OK"
 
+# Analyze src/usc
 printf " * Checking src/usc... "
 for file in ../src/usc/*.c; do
-	splint -I../include -DCONFIG_CLIENT_SPECIFIC=1 -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog +ignorequals -duplicatequals ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} -DCONFIG_CLIENT_SPECIFIC=1 ${SPLINT_OPTS} ${SPLINT_OPTS_EXTRA} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
 echo "   OK"
 
+# Analyze src/usd
 printf " * Checking src/usd... "
 for file in ../src/usd/*.c; do
-	splint -I../include -DCONFIG_DAEMON_SPECIFIC=1 -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog +ignorequals -duplicatequals ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} -DCONFIG_DAEMON_SPECIFIC=1 ${SPLINT_OPTS} ${SPLINT_OPTS_EXTRA} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
 echo "   OK"
 
+# Analyze src/use
 printf " * Checking src/use... "
 for file in ../src/use/*.c; do
-	splint -I../include -DCONFIG_EXEC_SPECIFIC=1 -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog +ignorequals -duplicatequals ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} -DCONFIG_EXEC_SPECIFIC=1 ${SPLINT_OPTS} ${SPLINT_OPTS_EXTRA} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
 echo "   OK"
 
+# Analyze src/usm
 printf " * Checking src/usm... "
 for file in ../src/usm/*.c; do
-	splint -I../include -D_POSIX_C_SOURCE=199309L -D_REENTRANT -Dtimer_t=unsigned -posix-lib -weak -redef -fcnuse -unrecog +ignorequals -duplicatequals ${file} 2> .splint_out
+	splint ${INCLUDES} ${DEFINES} ${SPLINT_OPTS} ${SPLINT_OPTS_EXTRA} ${file} 2> ${SPLINT_OUT}
 
 	if [ ${?} -ne 0 ]; then
-		cat .splint_out
+		cat ${SPLINT_OUT}
 		echo "File ${file} failed splint check."
-		rm -f .splint_out
+		rm -f ${SPLINT_OUT}
 		exit 1
 	fi
 done
@@ -82,5 +96,8 @@ echo "   OK"
 
 printf "\nDone.\n"
 
-rm -f .splint_out
+# Unlink output file
+rm -f ${SPLINT_OUT}
 
+# All good
+exit 0
